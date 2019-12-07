9d7f054decfba8d74c59d8c2aeacb55f251c03d6
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2019-12-07 23:57:30 +0100

    Dec 7 first solution

--- src/Dec7.jl
+++ src/Dec7.jl
@@ -1,5 +1,20 @@
-include("OpCodes5.jl")
-using .OpCodes5
+include("OpCodes.jl")
+using .OpCodes
 ps(x) = parse.(Int, split(x, ','))
 
-data = ps(readline("data/Dec5_data.txt"))
+function testsettings(settings, ics, input)
+    for i in eachindex(settings)
+        compute!(ics[i], [settings[i], input])
+        input = ics[i].output[1]
+    end
+    ics[end].output
+end
+
+test1 = ps("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0")
+ics = [IntComputer(test1) for i in 1:5]
+settings = [4,3,2,1,0]
+testsettings(settings, ics, 0)
+
+
+data = ps(readline("data/Dec7_data.txt"))
+abcde = [IntComputer(data) for i in 1:5]
