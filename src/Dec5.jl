e5542dc3053c47ddee9467f2c598b1aa0e8a8a02
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2019-12-07 17:56:13 +0100

    Change to use intcomputer explicitly

--- src/Dec5.jl
+++ src/Dec5.jl
@@ -2,9 +2,10 @@ include("OpCodes5.jl")
 using .OpCodes5
 ps(x) = parse.(Int, split(x, ','))
 
-data = ps(readline("data/Dec5_data.txt"))
-res = compute(data, 1)
-res.output
+ic = IntComputer(ps(readline("data/Dec5_data.txt")))
+
+compute!(ic, 1)
+ic.output
 
 
 compute(ps("3,9,8,9,10,9,4,9,99,-1,8"))
