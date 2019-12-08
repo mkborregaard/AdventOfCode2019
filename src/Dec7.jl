ff62eba164bc83e0223e0c0baf809749ac102bee
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2019-12-08 21:50:21 +0100

    wip attempt at 7-2

--- src/Dec7.jl
+++ src/Dec7.jl
@@ -1,20 +1,45 @@
-include("OpCodes.jl")
+includet("OpCodes.jl")
 using .OpCodes
-ps(x) = parse.(Int, split(x, ','))
 
-function testsettings(settings, ics, input)
+function testsettings(settings, data, input)
+    ics = [IntComputer(data) for i in 1:5]
+    addinput!.(ics, settings)
     for i in eachindex(settings)
-        compute!(ics[i], [settings[i], input])
-        input = ics[i].output[1]
+        compute!(ics[i], input)
+        input = getoutput(ics[i])[1]
     end
-    ics[end].output
+    input
 end
 
+# do it
+using Combinatorics
+data = readline("data/Dec7_data.txt")
+findmax(map(x->testsettings(x,data, 0), permutations(0:4)))
+
+# Question 2
+function circular(data, settings, input)
+    channels = [Channel{Int}(32) for i in 1:5]
+    ics = [IntComputer(data, channels[i], channels[i == 5 ? 1 : i + 1]) for i in 1:5]
+    addinput!.(ics, settings)
+    addinput!(ics[1], input)
+    for i in 1:5
+        @async compute!(ics[i])
+    end
+    @show [fetch(ics[i].output) for i in 1:5]
+    getoutput(ics[end])
+end
+
+settings = 9:-1:5
+data = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
+circular(data, settings, 0)
+
+# Test cases
 test1 = ps("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0")
 ics = [IntComputer(test1) for i in 1:5]
 settings = [4,3,2,1,0]
 testsettings(settings, ics, 0)
 
-
-data = ps(readline("data/Dec7_data.txt"))
-abcde = [IntComputer(data) for i in 1:5]
+test2 = ps("3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0")
+ics = [IntComputer(test2) for i in 1:5]
+settings = 0:4
+testsettings(settings, ics, 0)
