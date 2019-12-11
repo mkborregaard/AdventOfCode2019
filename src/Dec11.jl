5c54573e816f1fd17ddfd87feb0d9066472fa208
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2019-12-11 22:42:29 +0100

    some updates to dec 11

--- src/Dec11.jl
+++ src/Dec11.jl
@@ -2,19 +2,27 @@ include("Opcodes.jl")
 using .OpCodes
 using StaticArrays
 
-left =  @SMatrix [0  1;
-                 -1  0]
-right = @SMatrix [0 -1;
-                  1  0]
-dir =   @MVector [-1, 0]
-pos =   @MVector [0, 0]
-field = Set{typeof(pos)}()
+function run1(data)
+    left =  @SMatrix [0  1;
+                     -1  0]
+    right = @SMatrix [0 -1;
+                      1  0]
+    dir =   @SVector [-1, 0]
+    pos =   @SVector [0, 0]
+    field = Set{typeof(pos)}()
+    inp = Channel{Int}(1)
+    outp = Channel{Int}(1)
+    ic = IntComputer(data, inp, outp)
+    @async compute!(ic)
+    while !ic.finished[]
+        instr = pos ∈ field ? 1 : 0
+        put!(inp, instr)
+        color = take!(outp)
+        color == 1 && push!(field, pos)
+    #    put!(inp, instr)
+        dir = take!(outp) == 1 ? right * dir : left * dir
+        pos = pos + dir
+    end
+end
 
-inp = Channel{Int}(1)
-outp = Channel{Int}(2)
-
-ic = IntComputer(readline("data/Dec11_data.txt"), inp, outp)
-@async compute!(ic)
-put!(inp, 0)
-take!(outp)
-                 
+run1(readline("data/Dec11_data.txt"))
