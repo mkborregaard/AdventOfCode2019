692ee84a99fb116c6c1fc3fe87d866c9bbd638ce
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2019-12-12 00:51:05 +0100

    yay made it

--- src/Dec11.jl
+++ src/Dec11.jl
@@ -2,27 +2,37 @@ include("Opcodes.jl")
 using .OpCodes
 using StaticArrays
 
-function run1(data)
+function run1(data, startwhite = false)
     right =  @SMatrix [0  1;
                      -1  0]
     left = @SMatrix [0 -1;
                       1  0]
     dir =   @SVector [-1, 0]
     pos =   @SVector [0, 0]
-    field = Set{typeof(pos)}()
+    field = Dict{typeof(pos), Int}()
     inp = Channel{Int}(1)
     outp = Channel{Int}(1)
     ic = IntComputer(data, inp, outp)
     @async compute!(ic)
+    startwhite && (field[pos] = 1)
     while !ic.finished[]
-        instr = pos ∈ field ? 1 : 0
-        put!(inp, instr)
-        color = take!(outp)
-        color == 1 && push!(field, pos)
+        put!(inp, get(field, pos, 0))
+        field[pos] = take!(outp)
         dir = take!(outp) == 1 ? right * dir : left * dir
         pos = pos + dir
     end
-    length(field)
+    field
 end
 
-run1(readline("data/Dec11_data.txt"))
+# question 1
+f = run1(readline("data/Dec11_data.txt"))
+length(keys(f))
+
+# question 2 (plus an added arg to the function)
+f = run1(readline("data/Dec11_data.txt"), true)
+
+b = fill(NaN, 100, 100)
+for (k,v) in f
+    b[(k .+ 50)...] = v
+end
+heatmap(b, ylim = (40,60), xlim = (40, 100), yflip = true)
