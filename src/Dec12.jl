042eefa763908e47a345a178a55e95839c5659de
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2019-12-12 18:18:52 +0100

    Dec12 part 2

--- src/Dec12.jl
+++ src/Dec12.jl
@@ -26,13 +26,60 @@ function run1(data, nsteps)
     end
 
     sum([sum(abs.(pos[i])) .* sum(abs.(vel[i])) for i in eachindex(pos)])
-    #[sum(abs.(pos[i])) for i in 1:3]
 end
 
 data = readlines("data/Dec12_data.txt")
 run1(data, 1000)
 
 
+function run2(data, nsteps)
+    pos = [MVector{3}(parse.(Int, getv(x))) for x in data]
+    vel = [MVector{3}(0,0,0) for x in 1:length(pos)]
+    poss = Vector{typeof(pos)}()
+    vels = Vector{typeof(vel)}()
+
+    for step in 1:nsteps
+        for comb in combinations(eachindex(pos), 2)
+            for axis in 1:3
+                v,l = pos[comb[1]][axis], pos[comb[2]][axis]
+                rel = v == l ? (0, 0) : v > l ? (-1, 1) : (1, -1)
+                for i in 1:2
+                    vel[comb[i]][axis] += rel[i]
+                end
+            end
+        end
+
+        for i in eachindex(pos)
+            pos[i] .+= vel[i]
+        end
+        push!(poss, deepcopy(pos))
+        push!(vels, deepcopy(vel))
+    end
+
+    poss, vels #sum([sum(abs.(pos[i])) .* sum(abs.(vel[i])) for i in eachindex(pos)])
+end
+
+function findlag(v)
+    f = 1
+    while !isnothing(f)
+        f = findnext(==(v[1]), v, f+1)
+        v[1:f] == v[f .+ (1:f) .- 1] && return f-1
+    end
+end
+
+function findlags(poss)
+    ret = Int[]
+    for planet in 1:length(poss[1])
+        for axis in 1:length(poss[1][1])
+            p = [poss[step][planet][axis] for step in eachindex(poss)]
+            push!(ret, findlag(p))
+        end
+    end
+    ret
+end
+
+poss, vels = run2(data, 1000000)
+alls = lcm([findlags(poss); findlags(vels)]...)
 
 
 # test cases
