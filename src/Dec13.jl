2fd070832eed0665aa9aa48f9528be5c5ec91bf7
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2019-12-13 09:24:51 +0100

    wip first stab at 2

--- src/Dec13.jl
+++ src/Dec13.jl
@@ -22,3 +22,28 @@ field = fill(NaN, 21, 46)
 [field[(reverse(k).+1)...] = v for (k,v) in r1]
 using Plots
 heatmap(field)
+
+
+# question 2
+
+function run2(data)
+    inp, outp = Channel{Int}(1), Channel{Int}(3)
+    d = parse.(Int,split(data, ','))
+    d[1] = 2
+    ic = IntComputer(data, inp, outp)
+    @async compute!(ic)
+    field = zeros(Int, 46, 21)
+    score = 0
+    put!(inp,1)
+    while !ic.finished[]
+        x,y,el = [take!(outp) for i in 1:3]
+        if (x,y) == (-1, 0)
+            score = el
+        else
+            field[x+1, y+1] = el
+        end
+    end
+    close(inp)
+    score, count(==(2), values(field)), collect(inp)
+end
+s,c = run2(data)
