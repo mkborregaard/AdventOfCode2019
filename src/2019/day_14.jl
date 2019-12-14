fa594e30604750488170a758b2ac43ea8f8a8b4d
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2019-12-14 09:50:15 +0100

    Day 14 - 1

---
+++ src/2019/day_14.jl
@@ -0,0 +1,27 @@
+# https://adventofcode.com/2019/day/14
+
+data = readlines("data/2019/day_14.txt")
+
+reqs = Dict{String, Any}
+ingr, prods = split(line, " => ")
+ingrs = split(ingr, ",")
+prod_amount, prod = parse.([Int, String], split(prods, " "))
+for ing in ingrs
+    insert!(reqs, )
+for line in data
+
+
+requirements =
+
+
+
+
+function part_1(input)
+    nothing
+end
+@info part_1(input)
+
+function part_2(input)
+    nothing
+end
+@info part_2(input)
