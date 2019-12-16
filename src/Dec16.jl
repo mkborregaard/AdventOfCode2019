6367393caadca9422fabd50a35d1d389801afc7b
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2019-12-16 18:39:13 +0100

    experiments

--- src/Dec16.jl
+++ src/Dec16.jl
@@ -1,8 +1,23 @@
 getdata(data) = parse.(Int, split(data, ""))
 dats = getdata(readline("data/Dec16_data.txt"))
 
-l = length(dats)
-for i in eachindex(dats)
-pat = repeat([0,1,0,-1], inner = i, outer = 1 + l ÷ 4i)[2:l+1]
-res = getindex.(digits.(dats .* pat), Ref(1))
-dats .* pat
+pattern(n, i) =  (r = 1 + n ÷ i % 4; (3 - r) * ((1 + r) % 2))
+
+function run1(dats)
+    next = similar(dats)
+    for j in 1:100
+        for i in eachindex(next)
+            s = sum(n -> dats[n] * pattern(n,i), eachindex(dats))
+            next[i] = abs(digits(s)[1])
+        end
+        dats, next = next, dats
+    end
+    dats
+end
+
+using Profile
+Profile.init(10000000, 0.001)
+@profiler run1(dats)[1:8]
+
+using BenchmarkTools
+@btime run1($(dats))
